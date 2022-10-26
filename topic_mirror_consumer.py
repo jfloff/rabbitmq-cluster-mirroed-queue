149ef3fefa9d51315e825f59758de3d0220492de
Joao Loff <jfloff@gmail.com>
2022-10-26 12:26:52 +0100

    update

--- topic_mirror_consumer.py
+++ topic_mirror_consumer.py
@@ -3,33 +3,49 @@ import pika
 import sys
 
 port = int(sys.argv[1])
-EXCHANGE = 'rabbit1' if port == 5672 else 'rabbit2'
-QUEUE = 'write-home-timeline'
 creds = pika.PlainCredentials('admin', 'admin')
-
-#------------------------------
-# Receive message on Rabbit2
-#------------------------------
 rabbit2 = pika.ConnectionParameters(host='localhost', port=port, credentials=creds)
 
 # create connection and channel
 rabbit2_connection = pika.BlockingConnection(rabbit2)
 rabbit2_channel = rabbit2_connection.channel()
 
+EXCHANGE = 'notifications'
 rabbit2_channel.exchange_declare(exchange=EXCHANGE, exchange_type='topic')
 
-# result = channel.queue_declare('', exclusive=True)
-# queue_name = result.method.queue
-# rabbit2_channel.queue_declare(QUEUE, exclusive=True)
+queues = []
 
-rabbit2_channel.queue_bind(exchange=EXCHANGE, queue=QUEUE, routing_key=EXCHANGE)
+# if port == 5672:
+#   queues << {
+#     'name': 'write-home-timeline-eu'
+#     'routing_key': str(5672),
+#   }
+# elif port == 5673:
+#   queues << {
+#     'name': 'write-home-timeline-us'
+#     'routing_key': str(5673),
+#   }
+
+queues.append({
+  'name': 'write-home-timeline-eu',
+  'routing_key': str(5672),
+})
+queues.append({
+  'name': 'write-home-timeline-us',
+  'routing_key': str(5673),
+})
+
+# result = rabbit2_channel.queue_declare('', exclusive=True)
+# queue_name = result.method.queue
 
 def callback(ch, method, properties, body):
-  print(f" [{port}-{EXCHANGE}] [x] {method.routing_key}:{body.decode()}")
+  print(f" [x] [{EXCHANGE}:{method.routing_key}] Received: {body.decode()}")
 
-rabbit2_channel.basic_consume(queue=QUEUE, on_message_callback=callback, auto_ack=True)
+for queue in queues:
+  rabbit2_channel.queue_bind(exchange=EXCHANGE, queue=queue['name'], routing_key=queue['routing_key'])
+  rabbit2_channel.basic_consume(queue=queue['name'], on_message_callback=callback, auto_ack=True)
+  print(f" [*] Waiting for logs [{EXCHANGE}:{queue['routing_key']}--{queue['name']}]. To exit press CTRL+C")
 
-print(f' [{port}-{EXCHANGE}] [*] Waiting for logs. To exit press CTRL+C')
 rabbit2_channel.start_consuming()
 
 # close connection
--- topic_mirror_publisher.py
+++ topic_mirror_publisher.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python3
 import pika
+import sys
 from datetime import datetime
 
-EXCHANGE = 'rabbit1'
-QUEUE = 'write-home-timeline'
+port = int(sys.argv[1])
 creds = pika.PlainCredentials('admin', 'admin')
-message = str(datetime.now())
-
-#------------------------------
-# Publish message on Rabbit1
-#------------------------------
-rabbit1 = pika.ConnectionParameters(host='localhost', port=5672, credentials=creds)
+rabbit1 = pika.ConnectionParameters(host='localhost', port=port, credentials=creds)
 
 # create connection and channel
 rabbit1_connection = pika.BlockingConnection([rabbit1])
 rabbit1_channel = rabbit1_connection.channel()
 
 # declare topic
+EXCHANGE = 'notifications'
 rabbit1_channel.exchange_declare(exchange=EXCHANGE, exchange_type='topic')
 
 # publish message
-rabbit1_channel.basic_publish(exchange=EXCHANGE, routing_key=EXCHANGE, body=message)
-print(" [x] Sent %r:%r" % (QUEUE, message))
+message = str(datetime.now())
+rabbit1_channel.basic_publish(exchange=EXCHANGE, routing_key=str(port), body=message)
+print(f" [x] [{EXCHANGE}:{port}] Sent: {message}")
 
 # close connection
 rabbit1_channel.close()
